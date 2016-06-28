0e9aedcd88a3888593ddf91cd1a014cdcc38ec5e
dougperosa <doug.perosa@gmail.com>
2016-06-28 10:04:42 -0300

    Atualização

---
+++ projetos.php
@@ -0,0 +1,26 @@
+<br/>
+<div class="" style="background-color: fff;">
+    <div class="alert alert-info" align="center"><b>NOSSOS PROJETOS</b></div> 
+    <table style="width: 90%" align="center">
+        <tr>
+            <td><img src="imagens/moveis/moveis1.png" width="360" height="260"></td>
+            <td><img src="imagens/moveis/moveis2.png" width="360" height="260"></td>
+            <td><img src="imagens/moveis/moveis3.png" width="360" height="260"></td>
+        </tr>
+        <tr>
+            <td><img src="imagens/moveis/moveis4.png" width="360" height="260"></td>
+            <td><img src="imagens/moveis/moveis5.png" width="360" height="260"></td>
+            <td><img src="imagens/moveis/moveis6.png" width="360" height="260"></td>
+        </tr>
+        <tr>
+            <td><img src="imagens/moveis/moveis7.png" width="360" height="260"></td>
+            <td><img src="imagens/moveis/moveis8.jpg" width="360" height="260"></td>
+            <td><img src="imagens/moveis/moveis9.png" width="360" height="260"></td>
+        </tr>
+    </table>
+
+    <br/>
+    <div align="center">
+        <div class="alert" style="width: 300px"><b><a href="">PORTFOLIO COMPLETO</a></b></div>
+    </div>
+</div>
---
+++ rodape.php
@@ -0,0 +1,23 @@
+<div style="height: 300px; width: 100%; background-image: url(imagens/slide-02.jpg); border-radius: 6px;">
+    <br/><br/>
+    <div style="margin-top: 30px">
+        <table style="width: 80%" align="center">
+            <tr align="center">
+                <td style="width: 20%"><img src="imagens/local.png"></td>
+                <td style="width: 20%"><img src="imagens/fone.png"></td>
+                <td style="width: 20%"><img src="imagens/email.png"></td>
+                <td style="width: 20%"><img src="imagens/facebook.png"></td>
+            </tr>
+            <tr align="center">
+                <td><span style="background-color: fff; border-radius: 6px">Cidade - UF<br/>Endereço</span></td>
+                <td>(00) 9999-9999</td>
+                <td>email@email.com</td>
+                <td>facebook.com/pagina</td>
+            </tr>
+        </table>
+        
+    </div> 
+    <div align="right" style="margin-right: 15px; margin-top: 80px">
+        DESENVOLVIDO POR DOUGLAS PEROSA
+    </div>
+</div>
